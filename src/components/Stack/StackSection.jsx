f1385fc48204f2cc41df6d1ec536e92dd6245697
Gerard Camí <gerardcamigay@gmail.com>
2025-02-14 13:13:26 +0100

    Improve layout and styling in About and StackSection components for better responsiveness and visual consistency

--- src/components/Stack/StackSection.jsx
+++ src/components/Stack/StackSection.jsx
@@ -7,7 +7,11 @@ export const StackSection = ({ section }) => {
         {section.title}
       </h3>
       <ul
-        className={`grid auto-rows-fr gap-4 md:grid-cols-3 ${section.title === "Workstation" ? "grid-cols-1 lg:grid-cols-3" : "grid-cols-2 lg:grid-cols-5"} auto-rows`}
+        className={`grid auto-rows-fr gap-4 md:grid-cols-3 ${
+          section.title === "Workstation"
+            ? "grid-cols-1 lg:grid-cols-3"
+            : "grid-cols-2 lg:grid-cols-5"
+        } auto-rows`}
       >
         {section.items.map((item) => (
           <li key={item.name}>
@@ -20,16 +24,15 @@ export const StackSection = ({ section }) => {
               <picture className="flex h-[50px] w-[50px] items-center justify-center">
                 <img
                   src={item.image}
-                  alt={`${item.name} logo`}
+                  alt={`${item.name} ${
+                    section.title !== "Workstation" && "logo"
+                  } `}
                   className="h-full w-full object-contain"
                 />
               </picture>
               <h4 className="text-center text-base font-medium text-white lg:text-md">
                 {item.name}
               </h4>
-              <p className="hidden text-center text-sm text-gray-400">
-                {item.description}
-              </p>
 
               <div className="absolute right-2 top-2 rounded-full border border-[#fafafa4c] p-1 text-[#fafafa4c] transition-all group-hover:bg-[#fafafa2d]">
                 <ArrowUpRight size={15} />
